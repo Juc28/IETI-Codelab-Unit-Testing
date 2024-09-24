fd36f83a8ba727676fd5cdc3492fbf33d9f16ce4
Erika Juliana Castro Romero <118181224+Juc28@users.noreply.github.com>
2024-09-24 16:29:41 -0500

    Update WeatherReportService.java

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -2,25 +2,30 @@ package org.adaschool.Weather.service;
 
 import org.adaschool.Weather.data.WeatherApiResponse;
 import org.adaschool.Weather.data.WeatherReport;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-@Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "e2e16405cdaa073a95309d3349ee4ba1";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    private static final String API_KEY = "e2e16405cdaa073a95309d3349ee4ba1";
+
+    private final RestTemplate restTemplate;
+
+    public WeatherReportService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public WeatherReport getWeatherReport(double latitude, double longitude) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = API_URL + "?lat=" + latitude + "&lon=" + longitude + "&appid=" + API_KEY;
         WeatherApiResponse response = restTemplate.getForObject(url, WeatherApiResponse.class);
 
         WeatherReport report = new WeatherReport();
-        WeatherApiResponse.Main main = response.getMain();
-        report.setTemperature(main.getTemperature());
-        report.setHumidity(main.getHumidity());
+        if (response != null && response.getMain() != null) {
+            double temperatureInKelvin = response.getMain().getTemperature();
+            double temperatureInCelsius = temperatureInKelvin - 273.15;
+            report.setTemperature(temperatureInCelsius);
 
+            report.setHumidity(response.getMain().getHumidity());
+        }
         return report;
     }
 }
